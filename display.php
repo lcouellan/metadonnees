d8039feb9cb49f8e2c2f4b1df060239ffccf3643
root <lcouellan@gmail.com>
2016-12-14 17:08:08 +0100

    Ajout Flickr

--- display.php
+++ display.php
@@ -1,6 +1,51 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<!-- jQuery library (served from Google) -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+	<!-- bxSlider Javascript file -->
+	<script src="lib/Bxslider/jquery.bxslider.min.js"></script>
+	<!-- bxSlider CSS file -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link href="lib/Bxslider/jquery.bxslider.css" rel="stylesheet" />
+	<link rel="stylesheet" href="css/style.css" />
+	<link href='https://fonts.googleapis.com/css?family=Alegreya+Sans' rel='stylesheet' type='text/css'>
+	<script src="js/script.js"></script>
+	<script src="//cdn.ckeditor.com/4.5.7/standard/ckeditor.js"></script>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  		<![endif]-->
+  		<title>Metadonnées</title>
+  	</head>
+  	<body>
+  		<h2>Images proposées par Flickr</h2>
+  		<div id="gal-photos"></div>
+  		<?php
 
-$res = shell_exec("exiftool -h medias/images/".$_GET["nom"]);
-echo $res;
+		require_once("functions.php");
 
-echo '<figure><img src="medias/images/'.$_GET["nom"].'"></figure>';
+  		$json = explode('.',$_GET["nom"]);
+		$json = $json[0].".json";
+		$value = refresh($json);
+		echo "<p id='keywords'>";
+		$max = count($value[0]['XMP']['Subject']) -1;
+		foreach ($value[0]['XMP']['Subject'] as $key=>$keyword) {
+			$keyword = str_replace(' ', '%20', $keyword);
+			if($key != $max) { $keyword .= ","; }
+			echo $keyword;
+		}
+		echo "</p>";
+
+  		$res = shell_exec("exiftool -h medias/images/".$_GET["nom"]);
+  		echo $res;
+
+  		echo '<figure><img src="medias/images/'.$_GET["nom"].'"></figure>';
+  		?>
+  	</body>
+  	</html>
--- js/script.js
+++ js/script.js
@@ -1,4 +1,87 @@
+// Create the XHR object.
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+
+// Make the actual CORS request.
+function makeCorsRequest() {
+  // All HTML5 Rocks properties support CORS.
+  var tags = document.getElementById("keywords").innerText;
+  var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=fa6059cb7324c6a70fa980b93a0ff732&tags='+tags+'&format=json&nojsoncallback=1';
+  console.log(tags);
+  console.log(url);
+  var xhr = createCORSRequest('GET', url);
+  xhr.responseType = 'json';
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  };
+  // Response handlers.
+  xhr.onload = function() {
+    var displayGal = document.getElementById("gal-photos");
+    var json = xhr.response;
+    var link = document.createElement("a");
+    // Pour les photos de la galerie photos
+    if(document.getElementById("gal-photos")){
+      for(var i = 0; i < 4; i++)
+      {
+        var farm = json.photos.photo[i].farm;
+        var server = json.photos.photo[i].server;
+        var secret = json.photos.photo[i].secret;
+        var id = json.photos.photo[i].id;
+        var size = "b";
+        var figure = document.createElement('figure');
+        var legende = document.createElement('figcaption');
+        var img = document.createElement("img");
+        var link = document.createElement("a");
+        img.setAttribute("height","auto");
+        img.setAttribute("width","100%");
+        img.setAttribute("src", "https://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+"_"+size+".jpg");
+        link.setAttribute("title",json.photos.photo[i].title);
+        link.setAttribute("href","https://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+"_"+size+".jpg");
+        legende.innerHTML = json.photos.photo[i].title;
+        link.appendChild(img);
+        figure.appendChild(link);
+        figure.appendChild(legende);
+        displayGal.appendChild(figure);
+      }
+    }
+  };
+
+  xhr.onerror = function() {
+    alert('Woops, there was an error making the request.');
+  };
+
+  xhr.send();
+}
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+
 $(document).ready(function(){
+	makeCorsRequest();
 	$('.bxslider').bxSlider({
 		pagerCustom: '#bx-pager'
 	});
